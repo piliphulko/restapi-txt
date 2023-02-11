6cd3b957895cf31b98b7eafeee9d73b3f1d3485d
pilip <piliphulko25@gmail.com>
2023-02-12 00:06:13 +0300

    update error info

--- pkg/datelog/FuncFile.go
+++ pkg/datelog/FuncFile.go
@@ -14,28 +14,31 @@ func logFataIF(err error) {
 		log.Fatal(err)
 	}
 }
+func errInfoType(err error, typeData int) error {
+	return fmt.Errorf("%w | type: %s", err, DetailTypes[typeData].NameType)
+}
 
 func GetDataTypesFromFile(fileName string, typeData int) ([]AllTypes, error) {
 	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDONLY, os.FileMode(0755))
 	if err != nil {
-		return nil, err
+		return nil, errInfoType(err, typeData)
 	}
 	scanner := bufio.NewScanner(file)
 	slice := make([]AllTypes, 0)
 	for scanner.Scan() {
 		sliceType, err := DetailTypes[typeData].ScanType(scanner.Text())
 		if err != nil {
-			return nil, err
+			return nil, errInfoType(err, typeData)
 		}
 		slice = append(slice, sliceType)
 	}
 	if scanner.Err() != nil {
-		return nil, err
+		return nil, errInfoType(err, typeData)
 	}
 	if file.Close() != nil {
-		return nil, err
+		return nil, errInfoType(err, typeData)
 	}
-	return slice, nil
+	return slice, errInfoType(err, typeData)
 }
 
 func DataWarehouseDeployment(typeData int) (*os.File, *os.File, func(), error) {
@@ -44,22 +47,22 @@ func DataWarehouseDeployment(typeData int) (*os.File, *os.File, func(), error) {
 		settingsFile = os.O_CREATE | os.O_RDWR | os.O_APPEND
 	)
 	if err := deploymentMkdir(DetailTypes, typeData); err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, errInfoType(err, typeData)
 	}
 	fAdd, err := os.OpenFile(DetailTypes[typeData].LocationAddFile, settingsFile, fm)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, errInfoType(err, typeData)
 	}
 	fDel, err := os.OpenFile(DetailTypes[typeData].LocationDelFile, settingsFile, fm)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, nil, nil, errInfoType(err, typeData)
 	}
 	closeData := func() {
 		logFataIF(fAdd.Close())
 		logFataIF(fDel.Close())
 		WarehousingData(typeData)
 	}
-	return fAdd, fDel, closeData, err
+	return fAdd, fDel, closeData, nil
 }
 
 func WarehousingData(typeData int) {
@@ -95,7 +98,7 @@ func deploymentMkdir(m map[int]typeDetail, typeData int) error {
 		filepath.Dir(m[typeData].LocationStockMainFile))
 	for _, v := range slice {
 		if err := mkdirAll(v); err != nil {
-			return err
+			return errInfoType(err, typeData)
 		}
 	}
 	return nil
@@ -112,10 +115,10 @@ func CheckEndWarehousingData(typeData int) error {
 	case !(errors.Is(err0, os.ErrNotExist)) && !(errors.Is(err1, os.ErrNotExist)) && !(errors.Is(err2, os.ErrNotExist)):
 		fmt.Printf("there was an incorrect completion of writing to the main file: %s\n", DetailTypes[typeData].NameType)
 		if err := os.Remove(DetailTypes[typeData].LocationMainFile); err != nil {
-			return err
+			return errInfoType(err, typeData)
 		}
 		if err := os.Rename(DetailTypes[typeData].LocationStockMainFile, DetailTypes[typeData].LocationMainFile); err != nil {
-			return err
+			return errInfoType(err, typeData)
 		}
 		WarehousingData(typeData)
 	case (!(errors.Is(err1, os.ErrNotExist)) && (errors.Is(err2, os.ErrNotExist))) ||
