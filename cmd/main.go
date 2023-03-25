5fc7f451b05f4c31de1078a52a180ec21b0776f3
pilip <piliphulko25@gmail.com>
2023-03-25 23:34:42 +0300

    viper use

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	_ "github.com/piliphulko/restapi-txt/pkg/datatxt"
 	"github.com/piliphulko/restapi-txt/pkg/httpout"
 	_ "github.com/piliphulko/restapi-txt/pkg/privacy"
+	"github.com/spf13/viper"
 )
 
+func init() {
+	viper.SetConfigType("json")
+	viper.SetConfigFile("../config/config.json")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	}
+	if !viper.IsSet("localhost") {
+		log.Fatalln("localhost does not have config")
+	}
+}
+
 func main() {
-	http.ListenAndServe(":8080", httpout.StartRouter())
+	http.ListenAndServe(viper.GetString("localhost"), httpout.StartRouter())
 }
